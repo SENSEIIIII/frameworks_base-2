AICP/frameworks_base

core: move OmniJaws client to its new home
Also includes
	Grant default location permissions to OmniJaws
	Author: Ali B.
	Change-Id: I2988eea61414dafc94e52729b997d07f4673a9ad

Change-Id: I50349a910aa604959def70377da135c6d94c7774

core/java/com/android/internal/util/aicp/OmniJawsClient.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.android.systemui.omni;
+package com.android.internal.util.aicp;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -40,8 +40,6 @@ import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
-
-import com.android.systemui.R;
 
 public class OmniJawsClient {
     private static final String TAG = "SystemUI:OmniJawsClient";
@@ -86,10 +84,10 @@ public class OmniJawsClient {
     private static final String WEATHER_UPDATE = "org.omnirom.omnijaws.WEATHER_UPDATE";
     private static final String WEATHER_ERROR = "org.omnirom.omnijaws.WEATHER_ERROR";
 
-    private static final String AE_EXTRA_FRAGMENT_CLASS = "com.aicp.extras.extra.preference_fragment";
     private static final String AE_SETTINGSACTIVITY = "com.aicp.extras.SettingsActivity";
     private static final String SETTINGS_PACKAGE_NAME = "com.aicp.extras";
     private static final String WEATHER_SETTINGS = "com.aicp.extras.fragments.Weather";
+    private static final String EXTRA_SHOW_FRAGMENT = ":android:show_fragment";
 
     private static final DecimalFormat sNoDigitsFormat = new DecimalFormat("0");
 
@@ -249,7 +247,7 @@ public class OmniJawsClient {
         if (isOmniJawsServiceInstalled()) {
             Intent settings = new Intent(Intent.ACTION_MAIN);
             settings.setClassName(SETTINGS_PACKAGE_NAME, AE_SETTINGSACTIVITY);
-            settings.putExtra(AE_EXTRA_FRAGMENT_CLASS, WEATHER_SETTINGS);
+            settings.putExtra(EXTRA_SHOW_FRAGMENT, WEATHER_SETTINGS);
             return settings;
         }
         return null;

packages/SystemUI/src/com/android/systemui/omni/CurrentWeatherView.java
@@ -49,6 +49,7 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.aicp.OmniJawsClient;
 import com.android.systemui.R;
 import com.android.settingslib.Utils;
 

packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
@@ -46,8 +46,8 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.aicp.OmniJawsClient;
 import com.android.systemui.R;
-import com.android.systemui.omni.OmniJawsClient;
 import com.android.systemui.statusbar.phone.SettingsButton;
 import com.android.systemui.plugins.ActivityStarter;
 

SENSEIIIII/frameworks_base-2

core/java/com/android/internal/util/derp/OmniJawsClient.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.android.systemui.omni;
+package com.android.internal.util.derp;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -40,8 +40,6 @@ import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
-
-import com.android.systemui.R;
 
 public class OmniJawsClient {
     private static final String TAG = "SystemUI:OmniJawsClient";
@@ -86,10 +84,10 @@ public class OmniJawsClient {
     private static final String WEATHER_UPDATE = "org.omnirom.omnijaws.WEATHER_UPDATE";
     private static final String WEATHER_ERROR = "org.omnirom.omnijaws.WEATHER_ERROR";
 
-    private static final String AE_EXTRA_FRAGMENT_CLASS = "com.power.hub.extra.preference_fragment";
     private static final String AE_SETTINGSACTIVITY = "com.power.hub.SettingsActivity";
     private static final String SETTINGS_PACKAGE_NAME = "com.power.hub";
     private static final String WEATHER_SETTINGS = "com.power.hub.fragments.Weather";
+    private static final String EXTRA_SHOW_FRAGMENT = ":android:show_fragment";
 
     private static final DecimalFormat sNoDigitsFormat = new DecimalFormat("0");
 
@@ -249,7 +247,7 @@ public class OmniJawsClient {
         if (isOmniJawsServiceInstalled()) {
             Intent settings = new Intent(Intent.ACTION_MAIN);
             settings.setClassName(SETTINGS_PACKAGE_NAME, AE_SETTINGSACTIVITY);
-            settings.putExtra(AE_EXTRA_FRAGMENT_CLASS, WEATHER_SETTINGS);
+            settings.putExtra(EXTRA_SHOW_FRAGMENT, WEATHER_SETTINGS);
             return settings;
         }
         return null;

packages/SystemUI/src/com/android/systemui/omni/CurrentWeatherView.java
@@ -49,6 +49,7 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.derp.OmniJawsClient;
 import com.android.systemui.R;
 import com.android.settingslib.Utils;
 

packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
@@ -46,8 +46,8 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.derp.OmniJawsClient;
 import com.android.systemui.R;
-import com.android.systemui.omni.OmniJawsClient;
 import com.android.systemui.statusbar.phone.SettingsButton;
 import com.android.systemui.plugins.ActivityStarter;
 